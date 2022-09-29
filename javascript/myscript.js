7c32ef2c34f78f3b03c3fabd797d07eb922a7bab
Vitor C <vitorconceicaoferreira@hotmail.com>
2022-09-29 13:51:51 -0300

    Header finalizado e arquivo JavaScript adicionado

---
+++ javascript/myscript.js
@@ -0,0 +1,39 @@
+// Função para mostrar a hora(s) e minuto(s) na tela 
+
+function mostraHora() {
+
+var data = new Date(); // Obtém a hora atual
+
+// Guarda cada pedaço em uma variável
+
+var hora = data.getHours(); // 0-23
+var min = data.getMinutes(); // 0-59
+
+// Formata a hora
+var str_hora = hora + ':' + min;
+
+// Mostra o resultado
+document.write(str_hora);
+}
+
+//Função para mostra o dia, mês e ano
+
+function mostraDia(){
+
+var data = new Date();  // Obtém a data
+
+// Guarda cada pedaço em uma variável
+
+var dia = data.getDate();
+
+var mes = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto','Setembro','Outubro','Novembro','Dezembro');
+
+var ano = data.getFullYear();
+
+// Formata a data e a hora (note o mês + 1)
+var str_data = dia + ' de ' + mes[data.getMonth()] + ' de ' + ano;
+
+// Mostra o resultado
+document.write(str_data);
+
+}
