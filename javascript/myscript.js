40eb8abbd8e3c398d9d85bb2683d36430df918d0
Vitor C <vitorconceicaoferreira@hotmail.com>
2022-09-30 10:05:35 -0300

    Estilo da inserção de atividades finalizado

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,38 +2,39 @@
 
 function mostraHora() {
 
-var data = new Date(); // Obtém a hora atual
+    var data = new Date(); // Obtém a hora atual
 
-// Guarda cada pedaço em uma variável
+    // Guarda cada pedaço em uma variável
 
-var hora = data.getHours(); // 0-23
-var min = data.getMinutes(); // 0-59
+    var hora = data.getHours(); // 0-23
+    var min = data.getMinutes(); // 0-59
 
-// Formata a hora
-var str_hora = hora + ':' + min;
+    // Formata a hora
+    var str_hora = hora + ':' + min;
 
-// Mostra o resultado
-document.write(str_hora);
+    // Mostra o resultado
+    document.write(str_hora);
 }
 
 //Função para mostra o dia, mês e ano
 
 function mostraDia(){
 
-var data = new Date();  // Obtém a data
+    var data = new Date();  // Obtém a data
 
-// Guarda cada pedaço em uma variável
+    // Guarda cada pedaço em uma variável
 
-var dia = data.getDate();
+    var dia = data.getDate(); // Guarda o dia 
 
-var mes = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto','Setembro','Outubro','Novembro','Dezembro');
+    var mes = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto','Setembro','Outubro','Novembro','Dezembro');
 
-var ano = data.getFullYear();
+    var ano = data.getFullYear(); // Guarda o ano
 
-// Formata a data e a hora (note o mês + 1)
-var str_data = dia + ' de ' + mes[data.getMonth()] + ' de ' + ano;
+    // Formata a data e a hora (note o mês + 1)
 
-// Mostra o resultado
-document.write(str_data);
+    var str_data = dia + ' de ' + mes[data.getMonth()] + ' de ' + ano;
+
+    // Mostra o resultado
+    document.write(str_data);
 
 }
