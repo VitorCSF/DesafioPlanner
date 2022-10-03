f740b14af915e89d5751e4120ff024d80adde240
Vitor C <vitorconceicaoferreira@hotmail.com>
2022-10-03 12:57:11 -0300

    Ajustes no cabeçalho e estilos dos botões

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -9,16 +9,31 @@ function mostraHora() {
     var hora = data.getHours(); // 0-23
     var min = data.getMinutes(); // 0-59
 
+    if (min < 10) {
+
+        min = '0' + min;
+
+    }
+
+
     // Formata a hora
+
     var str_hora = hora + ':' + min;
 
     // Mostra o resultado
-    document.write(str_hora);
+
+    document.getElementById("hora__cabecalho").value=str_hora;
+
 }
+
+// Intervalo para a função mostraHora     
+
+    var timer=setInterval(mostraHora,1000);
+
 
 //Função para mostra o dia, mês e ano
 
-function mostraDia(){
+    function mostraDia(){
 
     var data = new Date();  // Obtém a data
 
